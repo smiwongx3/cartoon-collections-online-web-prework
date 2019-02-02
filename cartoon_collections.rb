21acd12568e6f0217acd2cbb51f084e6fa4e811d
Samantha Wong <smiwong920@gmail.com>
2019-02-02 20:00:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,6 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |element| element.capitalize +"!"}
 end
-summon_captain_planet
 
 def long_planeteer_calls(calls)
   calls.any? do |word|
