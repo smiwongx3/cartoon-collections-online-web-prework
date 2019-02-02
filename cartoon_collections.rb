1622995d11f64eea6eaa865dc72befa090152af4
Samantha Wong <smiwong920@gmail.com>
2019-02-02 20:16:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element| 
-    element.capitalize +"!"
+    element.capitalize + "!"
   end
 end
 
